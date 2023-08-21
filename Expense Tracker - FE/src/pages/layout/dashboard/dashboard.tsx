5ff3fa2680cb3e2880a993f9a1e704f4d84f4b54
Andjela Bolta Kovacic <a.boltakovacic@vegait.rs>
2023-08-21 10:59:45 +0200

    added ad transaction button

--- Expense Tracker - FE/src/pages/layout/dashboard/dashboard.tsx
+++ Expense Tracker - FE/src/pages/layout/dashboard/dashboard.tsx
@@ -2,26 +2,32 @@ import Container from '@mui/material/Container';
 import AmountDisplay from '../../../common/amount-display';
 import DataTable from '../../../common/table/table';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import NoticeCard from '../../../common/notice-card';
 
 function Dashboard() {
   return (
     <Container>
       <AmountDisplay />
+      <Box mt={2} display='flex' justifyContent='end'>
+        <Button variant='contained' color='secondary'>
+          Add Transaction
+        </Button>
+      </Box>
+
       <Box my={2}>
-        <Typography p={2} color="primary.main">
+        <Typography p={2} color='primary.main'>
           Last 5 income transactions
         </Typography>
         <DataTable />
       </Box>
       <Box my={2}>
-        <Typography p={2} color="primary.main">
+        <Typography p={2} color='primary.main'>
           Last 5 expense transactions
         </Typography>
         <DataTable />
       </Box>
-      <NoticeCard title="Weekly reminder" text="some text" />
+      <NoticeCard title='Weekly reminder' text='some text' />
     </Container>
   );
 }
